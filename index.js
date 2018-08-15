ea821b07e379123e52883d98eca8884476eec3d6
plexx2g@icloud.com <plexx2g@icloud.com>
2018-08-15 00:04:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(a,b) {
   return a + b ;
 }
+
+function subtract(a,b) {
+  return a - b ;
+}
+
+function multiply(a,b)j {
+  return a * b ;
+}
+
+function divide(a,b) {
+  return a / b ;
+}
